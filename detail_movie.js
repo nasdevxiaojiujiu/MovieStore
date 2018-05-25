687aa7617b05231433fd83d69e6b1d650cde1a1e
jadonsun <jadonsun@git.code.oa.com>
2018-05-25 08:25:13 +0800

    fix bug
    fix 页面传参bug

--- detail_movie.js
+++ detail_movie.js
@@ -10,7 +10,8 @@ DetailMovie.prototype = {
 
     init: function() {
         var self = this;
-        var key=UrlParm.parm("key");  
+        var org_key = self.getPar("key");
+        var key=unescape(org_key);  
         self.initMovieInfo(key);
         self.bindShangEvent();
     },
@@ -113,7 +114,26 @@ DetailMovie.prototype = {
                 }, "*");
             }
         });
+    },
+
+    getPar: function(par){
+        //获取当前URL
+        var local_url = document.location.href; 
+        //获取要取得的get参数位置
+        var get = local_url.indexOf(par +"=");
+        if(get == -1){
+            return false;   
+        }   
+        //截取字符串
+        var get_par = local_url.slice(par.length + get + 1);    
+        //判断截取后的字符串是否还有其他get参数
+        var nextPar = get_par.indexOf("&");
+        if(nextPar != -1){
+            get_par = get_par.slice(0, nextPar);
+        }
+        return get_par;
     }
+     
 }
 
 var movieObj=new DetailMovie();
